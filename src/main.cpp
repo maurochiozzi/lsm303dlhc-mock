82f734950771e0a75ffd7d8519abbafc381316b6
Chiozzi <mchiozzi95@gmail.com>
2021-11-07 21:17:28 -0300

    refactor main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
+/**
+ * File name: 			  main.cpp
+ * File description:	This file contains all necessary resources to mock the
+ *                    LSM303DHLC magnetic field sensor using I2C.
+ *
+ * Author name:			Mauro Chiozzi
+ *
+ * Reference: [1] - https://pdf1.alldatasheet.com/datasheet-pdf/view/505234/STMICROELECTRONICS/LSM303DLHC.html
+ *
+ */
+
 #include <Wire.h>
 #include <Arduino.h>
 
 // --------------------------------------------------------------------------------------------- //
-// Se usar em modo debug, setar como true
+// If debug is set to true, system will send thru default serial port messages
+// regarding status and informations
 #define DEBUG true
 
-union union_i16_ui8 { 
-    uint8_t ui8[6];
-    int16_t i16[3];
+// Auxiliar Union... Maybe it will not be required... Lets see
+union union_i16_ui8
+{
+  uint8_t ui8[6];
+  int16_t i16[3];
 };
 
 union_i16_ui8 magnetic_fields;
@@ -15,133 +29,216 @@ union_i16_ui8 magnetic_fields;
 // --------------------------------------------------------------------------------------------- //
 // I2C
 
+/**
+ * Define LSM303DLHC I2C address
+ *
+ * Reference: [1] section 5.1.3, p21
+ */
 #define LSM303DLHC_MOCK_ADD (0x1E)
 
-// Valor da requisição. Usado para o master pedir informações específicas via I2C
-uint8_t requisicao;
+/**
+ * Stores request value from master
+ */
+uint8_t ui8_request_value;
+
+/**
+ * Method name: 			    initialize
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void initialize();
+
+/**
+ * Method name: 			    initializeI2C
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void initializeI2C();
+
+/**
+ * Method name: 			    initializeDebug
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void initializeDebug();
+
+/**
+ * Method name: 			    routine
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void routine();
 
 // --------------------------------------------------------------------------------------------- //
-// Inicializacao do dispositivo
-void inicializacao();
-// Inicializacao da comunicação i2c
-void inicializaI2C();
-// Inicializacao do debug
-void inicializaDebug();
 
-// --------------------------------------------------------------------------------------------- //
-// Rotina que ficará sendo executada no código
-void rotina();
+/**
+ * Method name: 			    onRequest
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void onRequest();
 
-// --------------------------------------------------------------------------------------------- //
-// I2C
-// Função que será chamada quando o dispositivo for requisitado
-void quandoRequisitado();
-// Função que será chamada quando o dispositivo receber alguma informação
-void quandoReceber(int quantitadeBytes);
-// Consome a requisição (seta como 0 o valor da requisição) se ela for realizada com sucesso
-void consumirRequisicao();
-void escreverDoisBytesWire(int intParaEnviar);
+/**
+ * Method name: 			    onReceive
+ * Method description:
+ *
+ * Input params:			    ui8_size
+ *
+ * Output params:			    None
+ */
+void onReceive(int size);
 
-int main(){
-    inicializacao();
+/**
+ * Method name: 			    consumeRequest
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+void consumeRequest();
 
-    while(true){
-        rotina();
-    }
+/**
+ * Method name: 			    writeTwoBytes
+ * Method description:
+ *
+ * Input params:			    i16_bytes_to_send
+ *
+ * Output params:			    None
+ */
+void writeTwoBytes(int16_t i16_bytes_to_send);
+
+/**
+ * Method name: 			    main
+ * Method description:
+ *
+ * Input params:			    None
+ *
+ * Output params:			    None
+ */
+int main()
+{
+  initialize();
+
+  while (true)
+  {
+    routine();
+  }
 }
 
-void inicializacao(){
+void initialize()
+{
   init();
-    // inicializa a comunicação I2c
-    inicializaI2C();
-    // Inicializa o debug, se for setado true
-    inicializaDebug();
+  initializeI2C();
+  initializeDebug();
 
-    magnetic_fields.i16[0] = 1000;
-    magnetic_fields.i16[1] = 1000;
-    magnetic_fields.i16[2] = 500;
+  magnetic_fields.i16[0] = 1000;
+  magnetic_fields.i16[1] = 1000;
+  magnetic_fields.i16[2] = 500;
 }
 
-void rotina(){
-   #if DEBUG
-    Serial.println("olar");
-    delay(500);
-   #endif
+void routine()
+{
+#if DEBUG
+  Serial.println("Hello");
+  delay(500);
+#endif
 }
 
-void inicializaI2C(){
-    // Inicializações do I2C
-    Wire.begin(LSM303DLHC_MOCK_ADD);
-    Wire.onReceive(quandoReceber);
-    Wire.onRequest(quandoRequisitado);
+void initializeI2C()
+{
+  Wire.begin(LSM303DLHC_MOCK_ADD);
+  Wire.onReceive(onReceive);
+  Wire.onRequest(onRequest);
 }
 
-void inicializaDebug(){
-    // Se setado como debug, inicializa a serial
-    #if DEBUG
-        Serial.begin(115200);
-        Serial.println("Debugando...");
-    #endif
+void initializeDebug()
+{
+#if DEBUG
+  Serial.begin(115200);
+  Serial.println("Debugging...");
+#endif
 }
 
+void onReceive(int size)
+{
+  if (Wire.available())
+  {
+    ui8_request_value = Wire.read();
 
-void quandoReceber(int quantitadeBytes){
-    if(Wire.available()){
-        requisicao = Wire.read();
-
-        #if DEBUG
-            Serial.print("Requisicao recebida: ");
-            Serial.println(requisicao);
-        #endif
-    }
+#if DEBUG
+    Serial.print("Request received: ");
+    Serial.println(ui8_request_value);
+#endif
+  }
 }
 
-void quandoRequisitado(){
-    // Quando as requisições forem executadas com sucesso, ela é consumida depois.
+void onRequest()
+{
+  if (ui8_request_value == 0x00)
+  {
+#if DEBUG
+    Serial.print("Request received: ");
+    Serial.println(ui8_request_value);
+#endif
 
-    if(requisicao == 0x00){
-        #if DEBUG
-            Serial.print("Requisicao recebida: ");
-            Serial.println(requisicao);
-        #endif
-        
-        consumirRequisicao();     
-    }else if(requisicao == 0x01){
-         #if DEBUG
-            Serial.print("Requisicao recebida: ");
-            Serial.println(requisicao);
-        #endif
-        
-        consumirRequisicao();     
-    }else if(requisicao == 0x02){
-        #if DEBUG
-            Serial.print("Requisicao recebida: ");
-            Serial.println(requisicao);
-        #endif
-       
+    consumeRequest();
+  }
+  else if (ui8_request_value == 0x01)
+  {
+#if DEBUG
+    Serial.print("Request received: ");
+    Serial.println(ui8_request_value);
+#endif
 
-        consumirRequisicao();
-    }else if(requisicao == 0x03){
-        #if DEBUG
-            Serial.print("Requisicao recebida: ");
-            Serial.println(requisicao);
-        #endif
+    consumeRequest();
+  }
+  else if (ui8_request_value == 0x02)
+  {
+#if DEBUG
+    Serial.print("Request received: ");
+    Serial.println(ui8_request_value);
+#endif
 
-        escreverDoisBytesWire(magnetic_fields.i16[0]);
-        escreverDoisBytesWire(magnetic_fields.i16[1]);
-        escreverDoisBytesWire(magnetic_fields.i16[2]);
-        
+    consumeRequest();
+  }
+  else if (ui8_request_value == 0x03)
+  {
+#if DEBUG
+    Serial.print("Request received: ");
+    Serial.println(ui8_request_value);
+#endif
 
-        consumirRequisicao();
-    }
+    writeTwoBytes(magnetic_fields.i16[0]);
+    writeTwoBytes(magnetic_fields.i16[1]);
+    writeTwoBytes(magnetic_fields.i16[2]);
+
+    consumeRequest();
+  }
 }
 
-
-void consumirRequisicao(){
-    requisicao = 0x00;
+void consumeRequest()
+{
+  ui8_request_value = 0x00;
 }
 
-void escreverDoisBytesWire(int intParaEnviar){
-    Wire.write((intParaEnviar >> 8) & 0xFF);
-    Wire.write((intParaEnviar) & 0xFF);
+void writeTwoBytes(int16_t i16_bytes_to_send)
+{
+  Wire.write((i16_bytes_to_send >> 8) & 0xFF);
+  Wire.write((i16_bytes_to_send)&0xFF);
 }
